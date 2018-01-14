65bbcb6c213ef33271eda7cd035f4798d98bda89
joshfreak7 <joni.werren@gmail.com>
2018-01-14 01:25:15 +0100

    - Updated renderMovies - updated showMovieDetails

--- src/app/app.ts
+++ src/app/app.ts
@@ -37,76 +37,72 @@ function renderMovies() {
                 $('<button>')
                     .addClass('movie-item')
                     .text(movie.title + ' - Rating: ' + movie.rating + ' / 10 (' + movie.votes + ' votes)')
-                    .on('click', () => showDetails(model.getMovie(movie.id)))
+                    .on('click', () => showMovieDetails(model.getMovie(movie.id))),
+                $('<button>')
+                    .addClass('favorite-btn')
+                    .text('fav')
             );
     }
+}
+
+function showMovieDetails(movie) {
+
+    const $movieDetails = $('#movieDetails');
+    $movieDetails.html('');
+    $('<tr>')
+        .appendTo($movieDetails)
+        .addClass('table-striped')
+        .append(
+            $('<th>').text('Titel'),
+            $('<th>').text('Rating'),
+            $('<th>').text('Votes')
+        );
+    $('<tr>')
+        .appendTo($movieDetails)
+        .append(
+            $('<td>').text(movie.title),
+            $('<td>').text(movie.rating + ' / 10'),
+            $('<td>').text(movie.votes)
+        );
+    showDetails(model.getMovie(movie.id));
+}
+
+function getMovies(url) {
+    model.resetMovieList();
+
+    $.get(url, function(data) {
+        const movies = data.results;
+        for (const movie of movies) {
+            model.addMovie(movie);
+        }
+    });
 }
 
 function doSearch() {
     const query = $searchInput.val();
     const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + apiKey + '&query=' + query + '&language=de-CH';
-
-    model.resetMovieList();
-
-    $.get(url, function(data) {
-        const movies = data.results;
-        for (const movie of movies) {
-            model.addMovie(movie);
-        }
-    });
+    getMovies(url);
 }
 
 function showBestRated() {
     const url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=' + apiKey + '&language=de-CH';
-
-    model.resetMovieList();
-
-    $.get(url, function(data) {
-        const movies = data.results;
-        for (const movie of movies) {
-            model.addMovie(movie);
-        }
-    });
+    getMovies(url);
 }
 
 function showMostPopular() {
     const url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=' + apiKey + '&language=de-CH';
-
-    model.resetMovieList();
-
-    $.get(url, function(data) {
-        const movies = data.results;
-        for (const movie of movies) {
-            model.addMovie(movie);
-        }
-    });
+    getMovies(url);
 }
 
 function showUpcoming() {
     const url = 'https://api.themoviedb.org/3/movie/upcoming?api_key=' + apiKey + '&language=de-CH';
-
-    model.resetMovieList();
-
-    $.get(url, function(data) {
-        const movies = data.results;
-        for (const movie of movies) {
-            model.addMovie(movie);
-        }
-    });
+    getMovies(url);
 }
 
 function showFavorites() {
-
-    model.resetMovieList();
     // TODO
-    /*
-    $.get(url, function (data) {
-        const movies = data.results;
-        for (const movie of movies) {
-            model.addMovie(movie);
-        }
-    });
-   */
+    model.resetMovieList();
+
 }
 
 $(model).on('modelchange', () => {
